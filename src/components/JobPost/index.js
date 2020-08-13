291a482b085b1a106c380a6e808c4c2ac579e2b6
anthonygedeon <anthony.gedeonn@gmail.com>
2020-08-13 14:20:11 -0500

    api works now

--- src/components/JobPost/index.js
+++ src/components/JobPost/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import PublicIcon from '@material-ui/icons/Public';
 import QueryBuilderIcon from '@material-ui/icons/QueryBuilder';
@@ -10,25 +10,9 @@ const JobPost = () => {
 	const [items, setItems] = useState([]);
 
 	useEffect(() => {
-		fetch('https://jobs.github.com/positions.json', {
-			headers: {
-				accept:
-					'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-				'accept-language': 'en-US,en;q=0.9,la;q=0.8,fr;q=0.7',
-				'cache-control': 'max-age=0',
-				'if-none-match': 'W/"202312b4118c4ee41e95fad2bdc606bb"',
-				'sec-fetch-dest': 'document',
-				'sec-fetch-mode': 'navigate',
-				'sec-fetch-site': 'none',
-				'sec-fetch-user': '?1',
-				'upgrade-insecure-requests': '1',
-			},
-			referrerPolicy: 'no-referrer-when-downgrade',
-			body: null,
-			method: 'GET',
-			mode: 'no-cors',
-			credentials: 'include',
-		})
+		fetch(
+			'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
+		)
 			.then((res) => res.json())
 			.then(
 				(result) => {
@@ -50,29 +34,49 @@ const JobPost = () => {
 	} else if (!isLoaded) {
 		return <div>Loading...</div>;
 	} else {
+		// eslint-disable-next-line no-lone-blocks
 		return (
-			<Link to="/" className="job-post">
-				<img src="" alt="Company brand" className="job-post__image" />
-				<div className="job-post__details">
-					<div className="job-post__company">
-						<p className="job-post__company-name">Lev</p>
-						<h3 className="job-post__position">Senior Engineer</h3>
-						<button className="job-post__full-time">
-							Full time
-						</button>
-					</div>
-					<div className="job-post__posting">
-						<div className="job-post__location-wrapper">
-							<PublicIcon />
-							<p className="job-post__location">New York</p>
-						</div>
-						<div className="job-post__timestamp-wrapper">
-							<QueryBuilderIcon />
-							<p className="job-post__timestamp">5 days ago</p>
-						</div>
-					</div>
-				</div>
-			</Link>
+			<Fragment>
+				{items.map((item) => {
+					console.log(item);
+					return (
+						<Link to={item.id} className="job-post">
+							<img
+								src={item.company_logo}
+								alt="Company brand"
+								className="job-post__image"
+							/>
+							<div className="job-post__details">
+								<div className="job-post__company">
+									<p className="job-post__company-name">
+										{item.company}
+									</p>
+									<h3 className="job-post__position">
+										{item.title}
+									</h3>
+									<button className="job-post__full-time">
+										{item.type}
+									</button>
+								</div>
+								<div className="job-post__posting">
+									<div className="job-post__location-wrapper">
+										<PublicIcon />
+										<p className="job-post__location">
+											{item.location}
+										</p>
+									</div>
+									<div className="job-post__timestamp-wrapper">
+										<QueryBuilderIcon />
+										<p className="job-post__timestamp">
+											{new Intl.RelativeTimeFormat('en', { style: 'narrow' }).format(-1 * new Date(item.created_at).getDay(),'day')}
+										</p>
+									</div>
+								</div>
+							</div>
+						</Link>
+					);
+				})}
+			</Fragment>
 		);
 	}
 };
