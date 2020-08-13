07ef47658a20a3634f1da1fc4be897cafb8579ec
anthonygedeon <anthony.gedeonn@gmail.com>
2020-08-13 16:25:56 -0500

    users can click on a job

--- src/pages/jobContent.js
+++ src/pages/jobContent.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { JobPostsContext } from '../providers/JobPostsContext';
 
 const JobContent = ({ match }) => {
@@ -6,8 +7,20 @@ const JobContent = ({ match }) => {
     return (
         <JobPostsContext.Consumer>
             {(context) => {
-                const job = context.company.filter(job => job.id === match.params.job)
-                console.log(job.how_to_apply, job.description)
+                const [ job ] = context.company.filter(job => job.id === match.params.job)
+                console.log(job)
+
+                return (
+                    <div className="job-content">
+                        <Link to="/"><button>Back</button></Link>
+                        <img src={job.company_logo} alt=""/>
+                        <p>{job.type}</p>
+                        <p>{job.title}</p>
+                        <p>{job.how_to_apply}</p>
+                        <p>{job.location}</p>
+                        {job.description}
+                    </div>
+                );
             }}
         </JobPostsContext.Consumer>
     )
